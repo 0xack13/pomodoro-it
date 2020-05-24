a5a7a82cbdd076b747b4b2b686a5a8c85aef2367
MxShun <hosomism@gmail.com>
2020-05-25 07:56:03 +0900

    Update#1 Simpletimer

--- background.js
+++ background.js
@@ -1,13 +1,47 @@
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log('The color is green.');
-  });
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
-      })],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
+  let counter = 0;
+
+/*
+ * タイマー起動時の毎分の処理
+ */
+function tick(timeout){
+  console.log(++counter);
+  if(counter >= timeout){
+    let msg = timer.working ? "仕事終わり！" : "休憩終わり！";
+    chrome.notifications.create({
+      title: "タイマー終了",
+      message: msg,
+      type: "basic",
+      iconUrl: "images/get_started48.png"
+    });
+    timer.stop();
+  }
+}
+
+/*
+ * タイマーの処理
+ */
+var timer = new function Timer(){
+  this.working = true; // work中か、否か
+  this.running = false; // timerは動いているか、否か
+
+  this.start = function(){
+    this.running = true;
+    let timeout = this.working ? 1 : 3; // work中なら前者[分]、でなければ後者[分]
+    interval = setInterval(tick, 1000, timeout);
+  }
+  this.stop = function(){
+    this.running = false;
+    counter = 0; // カウンターリセット
+    this.working = !this.working; // work中か、否かの反転
+    clearInterval(interval);
+  }
+};
+
+/*
+ * ブラウザアクションボタンクリック時の処理
+ */
+chrome.browserAction.onClicked.addListener(function() {
+  if(!timer.running){
+    timer.start();
+  }
 });
